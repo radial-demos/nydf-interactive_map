a1c827275ddede3e03e63ab005b80bac0c6abda8
Darren Anderson <dranders@colorado.edu>
2017-10-11 16:38:11 -0700

    Assign object properties 'countryCode' and 'centroid' to each country in dataset. These properties are exported from a separate data file 'countries.js'.

--- scripts/convert-tsv-to-json.js
+++ scripts/convert-tsv-to-json.js
@@ -1,13 +1,17 @@
 'use strict';
 
+require('dotenv').config();
+const debug = require('debug')('nydf:convert-tsv-to-json');
 const path = require('path');
 const fs = require('fs-extra');
 const _ = require('lodash');
 
+const countries = require('../data_files/countries');
+
 const DATA_DIR = path.join(__dirname, '..', 'data_files');
 const COL_DEFS = [
   {
-    key: 'country', sourceIndex: 0, label: 'Country',
+    key: 'countryName', sourceIndex: 0, label: 'Country',
   },
   {
     key: 'areaLoss', sourceIndex: 2, label: 'Tree Cover Loss', units: 'MHa', isNumber: true,
@@ -35,6 +39,7 @@ function tsvExtractor(colDefsArg) {
     // trimming each string in the process
     const headers = rows.shift().split('\t').map(h => h.trim());
     rows.forEach((rowString) => {
+      if (!rowString.length) return;
       const row = rowString.split('\t').map(r => r.trim());
       // loop through all the column defs
       const dataRow = {};
@@ -52,8 +57,32 @@ function tsvExtractor(colDefsArg) {
   return { parse };
 }
 
-const TSV_DATA = fs.readFileSync(path.join(DATA_DIR, 'countries.csv'), { encoding: 'utf8' });
+function assignCountryCode(datasetArg, nameLookup) {
+  // make keys independent of case, spaces, and international characters
+  function encodeName(name) {
+    return _.deburr(name).toLowerCase().replace(/ /g, '');
+  }
+  // create a reverse-lookup object to find codes keyed on (encoded) names
+  const idLookup = {};
+  Object.keys(nameLookup).forEach((id) => { idLookup[encodeName(nameLookup[id])] = id; });
+  datasetArg.data.forEach((datum) => {
+    const countryCode = idLookup[encodeName(datum.countryName)];
+    if (!countryCode) {
+      debug(`NAME NOT FOUND: ${datum.countryName}`);
+      return;
+    }
+    datum.countryCode = countryCode;
+  });
+  return datasetArg;
+}
 
+// Read raw TSV data and parse into dataset object
+const TSV_DATA = fs.readFileSync(path.join(DATA_DIR, 'dataset.csv'), { encoding: 'utf8' });
 const te = tsvExtractor(COL_DEFS);
-const dataset = te.parse(TSV_DATA);
+const dataset = assignCountryCode(te.parse(TSV_DATA), countries.name);
+// Assign centroids from 'countries'
+dataset.data.forEach((datum) => {
+  if (!datum.countryCode) return;
+  datum.centroid = countries.centroid[datum.countryCode];
+});
 fs.writeJsonSync(path.join(DATA_DIR, 'dataset.json'), dataset);
